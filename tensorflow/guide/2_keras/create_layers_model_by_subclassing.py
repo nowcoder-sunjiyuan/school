40a3addd040d68f8a6b1b020574dfa7bffbcbf92
sunjiyuan <sjiyuan@nowcoder.com>
2024-05-24 22:36:14 +0800

    feat(tf学习): 层嵌套，层调用顺序思考

--- tensorflow/guide/2_keras/create_layers_model_by_subclassing.py
+++ tensorflow/guide/2_keras/create_layers_model_by_subclassing.py
@@ -2,18 +2,39 @@ import tensorflow as tf
 from tensorflow import keras
 
 class Linear(keras.layers.Layer):
-    def __init__(self, units=32, input_dim=32):
+    def __init__(self, units=32):
         super(Linear, self).__init__()
+        self.units = units
+
+    def build(self, input_shape):
         self.w = self.add_weight(
-            shape=(input_dim, units), initializer="random_normal", trainable=True
+            shape=(input_shape[-1], self.units),
+            initializer="random_normal",
+            trainable=True,
         )
-        self.b = self.add_weight(shape=(units,), initializer="zeros", trainable=True)
+        self.b = self.add_weight(
+            shape=(self.units,), initializer="random_normal", trainable=True
+        )
 
     def call(self, inputs):
         return tf.matmul(inputs, self.w) + self.b
 
+class MLPBlock(keras.layers.Layer):
+    def __init__(self):
+        super(MLPBlock, self).__init__()
+        self.linear_1 = Linear(32)
+        self.linear_2 = Linear(32)
+        self.linear_3 = Linear(1)
 
-x = tf.ones((2, 2))
-linear_layer = Linear(4, 2)
-y = linear_layer(x)
-print(y)
+    def call(self, inputs):
+        x = self.linear_1(inputs)
+        x = tf.nn.relu(x)
+        x = self.linear_2(x)
+        x = tf.nn.relu(x)
+        return self.linear_3(x)
+
+
+mlp = MLPBlock()
+y = mlp(tf.ones(shape=(3, 64)))  # The first call to the `mlp` will create the weights
+print("weights:", len(mlp.weights))
+print("trainable weights:", len(mlp.trainable_weights))
