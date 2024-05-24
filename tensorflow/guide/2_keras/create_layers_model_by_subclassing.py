e49928ed7806ec79c7e02f56f645134aeaa8d46a
sunjiyuan <sunjiyuan@nowcoder.com>
2024-05-24 09:55:52 +0800

    feat(tf学习): 测试

---
+++ tensorflow/guide/2_keras/create_layers_model_by_subclassing.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow import keras
+
+class Linear(keras.layers.Layer):
+    def __init__(self, units=32, input_dim=32):
+        super(Linear, self).__init__()
+        self.w = self.add_weight(
+            shape=(input_dim, units), initializer="random_normal", trainable=True
+        )
+        self.b = self.add_weight(shape=(units,), initializer="zeros", trainable=True)
+
+    def call(self, inputs):
+        return tf.matmul(inputs, self.w) + self.b
+
+
+x = tf.ones((2, 2))
+linear_layer = Linear(4, 2)
+y = linear_layer(x)
+print(y)
